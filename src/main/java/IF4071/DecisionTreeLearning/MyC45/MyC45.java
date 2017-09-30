9aea67d7355e2b6f9f1e3e7a7434920cf17b4322
Ali Akbar <aliakbar@Alis-MacBook-Pro.local>
2017-09-30 21:12:12 +0700

    Add distributionForInstance method

--- src/main/java/IF4071/DecisionTreeLearning/MyC45/MyC45.java
+++ src/main/java/IF4071/DecisionTreeLearning/MyC45/MyC45.java
@@ -6,6 +6,7 @@ import weka.core.*;
 public class MyC45 implements Classifier {
     private MyC45ClassifierTree root;
 
+    @Override
     public void buildClassifier(Instances instances) throws Exception {
         getCapabilities().testWithFail(instances);
 
@@ -14,10 +15,17 @@ public class MyC45 implements Classifier {
         root.buildClassifier(instances);
     }
 
+    @Override
     public double classifyInstance(Instance instance) throws Exception {
         return root.classifyInstance(instance);
     }
 
+    @Override
+    public double[] distributionForInstance(Instance instance) throws Exception {
+        return new double[0];
+    }
+
+    @Override
     public Capabilities getCapabilities() {
         Capabilities result = new Capabilities((CapabilitiesHandler) this);
         result.disableAll();
